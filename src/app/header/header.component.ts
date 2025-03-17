0cda461a57e5c2981bcf2f25675768b3d4a977b8
Walo <wailbenelhassane@gmail.com>
2025-03-17 12:37:21 +0000

    feat: Initial landing carousel

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+    selector: 'app-header',
+    imports: [],
+    templateUrl: './header.component.html',
+    standalone: true,
+    styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
