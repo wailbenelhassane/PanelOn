4a87f4c3c5b343bd39fb85d4520dee84edd908e3
edumarrero <edumarreroglezz@gmail.com>
2025-03-20 15:56:14 +0100

    WIP: feat(payments): subscriptions_plans

--- src/app/pages/subscription-plans/subscription-plans.component.ts
+++ src/app/pages/subscription-plans/subscription-plans.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {PremiumDescriptionComponent} from '../../components/premium-description/premium-description.component';
+import {
+  PremiumPlansComparisonComponent
+} from '../../components/premium-plans-comparison/premium-plans-comparison.component';
+import {HeaderBacklinkComponent} from '../../components/header-backlink/header-backlink.component';
+import {FreePlanComponent} from '../../components/free-plan/free-plan.component';
 import {PremiumPlanMonthlyComponent} from '../../components/premium-plan-monthly/premium-plan-monthly.component';
 
 @Component({
   selector: 'app-subscription-plans',
-  imports: [PremiumPlanMonthlyComponent],
+  imports: [PremiumDescriptionComponent, PremiumPlansComparisonComponent, HeaderBacklinkComponent, FreePlanComponent, PremiumPlanMonthlyComponent],
   templateUrl: './subscription-plans.component.html',
   styleUrl: './subscription-plans.component.scss'
 })
 export class SubscriptionPlansComponent {
-
+  freePlan = {
+    title: 'Prefer to stick with the free version?',
+    description: 'No problem! Keep enjoying community comics. When you\'re ready to upgrade, we\'ll be here.',
+  }
 }
