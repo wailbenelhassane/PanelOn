482882ee6e456ef4b4544923dac501acdbbafae8
juanfradelrm <juanfradelrm@gmail.com>
2025-03-21 16:35:58 +0100

    Fire base added and implemented.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,52 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
 import { CommonModule } from '@angular/common';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, CommonModule],
-  template: `
-    <app-header></app-header>
-    <router-outlet></router-outlet>
-  `,
-  styleUrls: ['./app.component.scss']
+  imports: [CommonModule]
 })
 export class AppComponent {
-  title = 'PanelOn';
+  comics$;
+  users$;
+  characters$;
+  news$;
+  donations$;
+  payments$;
+  genres$;
+
+  constructor(private appService: AppService) {
+    this.comics$ = this.appService.getComics();
+    this.users$ = this.appService.getUsers();
+    this.characters$ = this.appService.getCharacters();
+    this.news$ = this.appService.getNews();
+    this.donations$ = this.appService.getDonations();
+    this.payments$ = this.appService.getPayments();
+    this.genres$ = this.appService.getGenres();
+
+    this.comics$.subscribe(comics => {
+      console.log('Lista de cómics:', comics);
+    });
+    this.users$.subscribe(users => {
+      console.log('Lista de usuarios:', users);
+    });
+    this.characters$.subscribe(characters => {
+      console.log('Lista de personajes:', characters);
+    });
+    this.news$.subscribe(news => {
+      console.log('Lista de noticias:', news);
+    });
+    this.donations$.subscribe(donations => {
+      console.log('Lista de donaciones:', donations);
+    });
+    this.payments$.subscribe(payments => {
+      console.log('Lista de pagos:', payments);
+    });
+    this.genres$.subscribe(genres => {
+      console.log('Lista de géneros:', genres);
+    });
+  }
 }
---
+++ src/environments/enviroment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCQqzf1Ue3v0WtdWNQ9GkP-TTta4eoW8hI",
+    authDomain: "panelon-fb7af.firebaseapp.com",
+    projectId: "panelon-fb7af",
+    storageBucket: "panelon-fb7af.firebasestorage.app",
+    messagingSenderId: "539993197273",
+    appId: "1:539993197273:web:bd60dd54cfcc9f2c38ccc8"
+  }
+};
