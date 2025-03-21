3030cfd85995601bace033e99b15a27c5b8e424d
edumarrero <edumarreroglezz@gmail.com>
2025-03-21 05:20:48 +0100

    WIP: feat(payments): subscriptions_plans

--- src/app/components/premium-plans-comparison/premium-plans-comparison.component.ts
+++ src/app/components/premium-plans-comparison/premium-plans-comparison.component.ts
@@ -2,10 +2,11 @@ import {Component} from '@angular/core';
 import {PremiumPlanMonthlyComponent} from '../premium-plan-monthly/premium-plan-monthly.component';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
 import {PremiumDescriptionComponent} from '../premium-description/premium-description.component';
+import {ButtonComponent} from '../button/button.component';
 
 @Component({
   selector: 'app-premium-plans-comparison',
-  imports: [PremiumPlanMonthlyComponent, CommonModule, PremiumDescriptionComponent],
+  imports: [PremiumPlanMonthlyComponent, CommonModule, ButtonComponent],
   templateUrl: './premium-plans-comparison.component.html',
   styleUrl: './premium-plans-comparison.component.scss'
 })
@@ -14,16 +15,24 @@ export class PremiumPlansComparisonComponent {
     title: 'Monthly Plan – Total Flexibility',
     description: 'Try the service with no commitments and pay month by month. Perfect if you want to explore everything we offer before making a long-term decision.',
     backgroundSection: '#EEEEEE',
-    backgroundPrice: '#999',
-    price: '9.99€'
+    buttonTitle: '9.99€',
+    buttonTextColor: 'black',
+    backgroundColorButton: '#EEEEEE',
+    backgroundColorHoverButton: '#B8B8B8',
+    buttonWidth: '4vw',
+    buttonHeight: '4vh',
   }
 
   planYearlyComponent = {
     title: 'Annual Plan – Biggest Savings',
     description: 'If you\'re a true comic fan, this is the best choice. Subscribe for a full year and get an exclusive discount, ensuring a year of unlimited adventures.',
     backgroundSection: '#FFB7A5',
-    backgroundPrice: '#E76F51',
-    price: '99.99€'
+    buttonTitle: '99.99€',
+    buttonTextColor: 'black',
+    backgroundColorButton: '#E76F51',
+    backgroundColorHoverButton: '#E66041',
+    buttonWidth: '4vw',
+    buttonHeight: '4vh',
   }
 
   imagePath = "/PanelOn/src/assets/plan-versus.svg"
