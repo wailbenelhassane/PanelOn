36d361a7f2d92816effd5177bb7d74e38241899e
Walo <wailbenelhassane@gmail.com>
2025-03-22 16:31:47 +0000

    feat: Add character image

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import {CharacterPageComponent} from './pages/character-page/character-page.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
+  { path: 'character', component: CharacterPageComponent },
   { path: '**', redirectTo: '' }
 ];
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
     selector: 'app-header',
-    imports: [],
+  imports: [
+    RouterLink
+  ],
     templateUrl: './header.component.html',
     standalone: true,
     styleUrl: './header.component.scss'
