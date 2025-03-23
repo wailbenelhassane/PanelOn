2d2d6c3c33c67b3afacdf725d7ccea4f8c6a8280
edumarrero <edumarreroglezz@gmail.com>
2025-03-23 03:11:06 +0100

    feat(payments): DONE

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -1,2 +1,7 @@
 import express from 'express';
-import cors from 'cors';
+import subscriptionRouter from './routes/subscription-management'
+
+const app = express();
+
+app.use(express.json());
+app.use('/subscriptions', subscriptionRouter);
