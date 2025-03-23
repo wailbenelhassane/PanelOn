56515d2de2482082f78ebd16bbf48f403f5452d0
edumarrero <edumarreroglezz@gmail.com>
2025-03-23 03:26:56 +0100

    feat(payments): DONE

--- src/app/pages/subscription-plans/subscription-plans.component.ts
+++ src/app/pages/subscription-plans/subscription-plans.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 import {PremiumDescriptionComponent} from '../../components/premium-description/premium-description.component';
 import {
   PremiumPlansComparisonComponent
 } from '../../components/premium-plans-comparison/premium-plans-comparison.component';
 import {HeaderBacklinkComponent} from '../../components/header-backlink/header-backlink.component';
 import {FreePlanComponent} from '../../components/free-plan/free-plan.component';
-import {PremiumPlanMonthlyComponent} from '../../components/premium-plan-monthly/premium-plan-monthly.component';
 import {ButtonComponent} from '../../components/button/button.component';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-subscription-plans',
@@ -19,5 +19,17 @@ export class SubscriptionPlansComponent {
     title: 'Prefer to stick with the free version?',
     description: 'No problem! Keep enjoying community comics. When you\'re ready to upgrade, we\'ll be here.',
     textButton: 'FREE'
+  }
+
+  constructor(private router: Router) {}
+
+  @Output() buttonClick = new EventEmitter<void>()
+
+  onClick() {
+    this.buttonClick.emit();
+  }
+
+  goHome() {
+    this.router.navigate(['/']);
   }
 }
