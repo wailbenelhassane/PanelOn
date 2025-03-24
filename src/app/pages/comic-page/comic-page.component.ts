d63a4847c69f431307afdf4804ced3f8816c9619
Walo <wailbenelhassane@gmail.com>
2025-03-24 14:49:10 +0000

    feat: Add comic characters

--- src/app/pages/comic-page/comic-page.component.ts
+++ src/app/pages/comic-page/comic-page.component.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import {HeaderComponent} from '../../components/header/header.component';
 import {ComicCoverComponent} from '../../components/comic-cover/comic-cover.component';
 import {ComicDescriptionComponent} from '../../components/comic-description/comic-description.component';
+import {CharacterCardComponent} from '../../components/character-card/character-card.component';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-comic-page',
   imports: [
     HeaderComponent,
     ComicCoverComponent,
-    ComicDescriptionComponent
+    ComicDescriptionComponent,
+    CharacterCardComponent,
+    NgForOf
   ],
   templateUrl: './comic-page.component.html',
   styleUrl: './comic-page.component.scss'
@@ -19,4 +23,27 @@ export class ComicPageComponent {
   author: string = "DC Comics";
   genre: string = "Superhero, Romance, Adventure";
   releaseDate: string = "March 1958";
+
+  characters = [
+    {
+      imageUrl: 'https://cdn.marvel.com/content/1x/005smp_com_crd_01.jpg',
+      superheroName: 'Spider-Man',
+      characterName: 'Peter Parker'
+    },
+    {
+      imageUrl: 'https://cdn.marvel.com/content/1x/032kgp_com_crd_01.jpg',
+      superheroName: 'KingPin',
+      characterName: 'Wilson Fisk'
+    },
+    {
+      imageUrl: 'https://cdn.marvel.com/content/1x/002irm_com_crd_01.jpg',
+      superheroName: 'Iron Man',
+      characterName: 'Tony Stark'
+    },
+    {
+      imageUrl: 'https://cdn.marvel.com/content/1x/003cap_com_crd_01.jpg',
+      superheroName: 'Captain America',
+      characterName: 'Steve Rogers'
+    }
+  ];
 }
