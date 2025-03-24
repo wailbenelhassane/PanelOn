d509037a94eb4b3e5e0203debbab62cd0ccb3361
Alberto José Rodríguez Ruano <alberto.rodriguez.2708@gmail.com>
2025-03-24 19:36:38 +0000

    Feat: Add login and register/sign up pages.

---
+++ src/app/pages/register-page/register-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-page',
+  imports: [],
+  templateUrl: './register-page.component.html',
+  styleUrl: './register-page.component.scss'
+})
+export class RegisterPageComponent {
+  imageSource: string = 'https://1.bp.blogspot.com/-j7pelIBKTn0/XacCFsSjsII/AAAAAAAAf_Y/rvpqFruHKj8Hym6PS7n7P0V3J3nmHvs5wCLcBGAsYHQ/s1600/05.jpg';
+  logoSource: string = '';
+}
