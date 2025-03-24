931173b241868e76e4e0a58d7dc7ec412de6295f
edumarrero <edumarreroglezz@gmail.com>
2025-03-24 14:35:00 +0100

    feat(news): WIP!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,11 @@ import {
 } from './components/premium-plans-comparison/premium-plans-comparison.component';
 import {SubscriptionPlansComponent} from './pages/subscription-plans/subscription-plans.component';
 import {PaymentPageComponent} from './pages/payment-page/payment-page.component';
+import {ArticlePageComponent} from './pages/article-page/article-page.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
+  { path: 'news', component: ArticlePageComponent },
   { path: 'subscription-plans', component: SubscriptionPlansComponent},
   { path: 'payment', component: PaymentPageComponent},
   { path: '**', redirectTo: '' }
