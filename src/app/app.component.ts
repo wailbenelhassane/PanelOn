e31a6794ef7db4efbca851f85a3071f571b27e4d
Walo <wailbenelhassane@gmail.com>
2025-03-25 20:01:49 +0000

    fix: failing imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AppService } from './app.service';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
-  template: `
+  imports: [RouterOutlet,CommonModule],
+    template: `
     <router-outlet></router-outlet>
-  `,
+    `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
