7a5f00ad6126c85157820f82395915df08baf1d9
Walo <wailbenelhassane@gmail.com>
2025-03-27 18:25:57 +0000

    fix: header effect

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,37 @@
-import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import { Component, HostListener } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-header',
   imports: [
-    RouterLink
+    RouterLink,
+    NgClass
   ],
-    templateUrl: './header.component.html',
-    standalone: true,
-    styleUrl: './header.component.scss'
+  templateUrl: './header.component.html',
+  standalone: true,
+  styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  private lastScroll = 0;
+  public isVisible = true;
 
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event) {
+    let currentScroll: number;
+    currentScroll = window.scrollY;
+
+    if (currentScroll <= 0) {
+      this.isVisible = true;
+      return;
+    }
+
+    if (currentScroll > this.lastScroll && currentScroll > 100) {
+      this.isVisible = false;
+    } else if (currentScroll < this.lastScroll) {
+      this.isVisible = true;
+    }
+
+    this.lastScroll = currentScroll;
+  }
 }
